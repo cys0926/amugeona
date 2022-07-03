106ebd6fdf684200e4f7e788838c404841063349
cys0926 <108395686+cys0926@users.noreply.github.com>
2022-07-03 11:36:55 +0900

    fire

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,44 @@
+import 'package:amugeona/const/colors.dart';
 import 'package:amugeona/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'sunflower',
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            color: Colors.white,
+            fontFamily: 'parisienne',
+            fontSize: 80.0,
+          ),
+          headline2: TextStyle(
+            color: Colors.white,
+            fontSize: 50.0,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyText1: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+          ),
+          bodyText2: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: const HomeScreen(),
     ),
   );
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:amugeona/const/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rounded_date_picker/flutter_rounded_date_picker.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -15,22 +17,104 @@ class _HomeScreenState extends State<HomeScreen> {
       extendBodyBehindAppBar: true,
       body: Stack(
         children: [
+          const _MainPicture(),
           _TopPart(
             onHeartPressed: onHeartPressed,
             onPicturePressed: onPicturePressed,
             onDatePressed: onDatePressed,
           ),
-          _MainPicture(),
         ],
       ),
     );
   }
 
-  void onHeartPressed() {}
+  void onHeartPressed() {
+    showRoundedDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1997),
+      lastDate: DateTime.now(),
+      imageHeader: const AssetImage("asset/img/date_picker_image.jpg"),
+      description: "처음 만난 날짜를 선택해주세요.",
+      styleDatePicker: MaterialRoundedDatePickerStyle(
+        textStyleDayButton: const TextStyle(
+          fontSize: 30.0,
+          color: Colors.white,
+          fontWeight: FontWeight.w500,
+        ),
+        textStyleYearButton: const TextStyle(
+          fontSize: 30.0,
+          color: Colors.white,
+          fontWeight: FontWeight.w500,
+        ),
+        paddingDateYearHeader:
+            const EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+        textStyleMonthYearHeader: const TextStyle(
+          fontSize: 28,
+          color: PRIMARY_COLOR,
+          fontWeight: FontWeight.bold,
+        ),
+        sizeArrow: 20,
+        colorArrowNext: PRIMARY_COLOR,
+        colorArrowPrevious: PRIMARY_COLOR,
+        paddingMonthHeader: const EdgeInsets.symmetric(vertical: 23),
+        textStyleDayHeader: const TextStyle(
+          fontSize: 15.0,
+          color: PRIMARY_COLOR,
+          fontWeight: FontWeight.w900,
+        ),
+        textStyleCurrentDayOnCalendar: const TextStyle(
+          fontSize: 16,
+          color: PRIMARY_COLOR,
+          fontWeight: FontWeight.bold,
+        ),
+        textStyleDayOnCalendar: TextStyle(
+          fontSize: 16.0,
+          color: Colors.black.withOpacity(0.8),
+          fontWeight: FontWeight.w400,
+        ),
+        textStyleDayOnCalendarSelected: const TextStyle(
+          fontSize: 16.0,
+          color: Colors.white,
+          fontWeight: FontWeight.w500,
+        ),
+        textStyleDayOnCalendarDisabled: TextStyle(
+          fontSize: 16.0,
+          color: Colors.grey.withOpacity(0.2),
+        ),
+        paddingDatePicker: const EdgeInsets.symmetric(horizontal: 20.0),
+        marginLeftArrowPrevious: 16,
+        marginTopArrowPrevious: 16,
+        marginTopArrowNext: 16,
+        marginRightArrowNext: 16,
+        textStyleButtonAction: TextStyle(
+          fontSize: 16.0,
+          color: Colors.black.withOpacity(0.5),
+        ),
+        textStyleButtonPositive: const TextStyle(
+          fontSize: 16.0,
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+        textStyleButtonNegative: TextStyle(
+          fontSize: 16.0,
+          color: Colors.black.withOpacity(0.5),
+        ),
+        decorationDateSelected: const BoxDecoration(
+          color: PRIMARY_COLOR,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
 
-  void onPicturePressed() {}
+  void onPicturePressed() {
+    print('onPicturePressed');
+  }
 
-  void onDatePressed() {}
+  void onDatePressed() {
+    print('onDatePressed');
+  }
 }
 
 class _MainPicture extends StatelessWidget {
@@ -38,9 +122,10 @@ class _MainPicture extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Opacity(
-      opacity: 0.3,
-      child: Image.asset('asset/img/middle_image.png'),
+    return Image.asset(
+      'asset/img/middle_image.png',
+      color: Colors.white.withOpacity(0.5),
+      colorBlendMode: BlendMode.modulate,
     );
   }
 }
@@ -96,10 +181,12 @@ class _TopPart extends StatelessWidget {
         horizontal: 8.0,
       ),
       child: IconButton(
+        onPressed: onPressed,
         iconSize: 32.0,
         color: PRIMARY_COLOR,
-        onPressed: onPressed,
-        icon: Icon(iconData),
+        icon: Icon(
+          iconData,
+        ),
       ),
     );
   }
